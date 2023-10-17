c50553368a5abfb3f549574f52a203745a5f95df
Jason Kennelly <tootle_likes_code@hotmail.com>
2023-10-17 12:46:35 +0100

    Added Questionnaires Index Page

---
+++ ExeterUniTask.Web/Pages/Questionnaires/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExeterUniTask.Web.Pages.Questionnaires;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
